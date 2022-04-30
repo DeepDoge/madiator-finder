cda04a9ef2d277847c784dfcd21a85f7a2af36bb
Shiba <44804845+DeepDoge@users.noreply.github.com>
2022-04-30 18:27:55 +0000

    🍙 updated leaderboard text

--- src/routes/leaderboard.svelte
+++ src/routes/leaderboard.svelte
@@ -8,10 +8,15 @@
 
 <div class="content">
     <h1>Leaderboard</h1>
-    <h2>What is Leaderboard?</h2>
     <p>Leaderboard shows the top public contributers of the <b>Madiator Finder</b> community database.</p>
-    <h2>How to become a public contributer?</h2>
-    <p>You can become contributer by installing our <b>Watch On LBRY</b> extension and enable the . </p>
+    <p>Be the one who first finds a <b>YouTube</b> Video/Channel that is also on <b>Odysee</b>, earn the points!</p>
+    <h2>How to earn points?</h2>
+    <ul>
+        <li>Install <a href="https://chrome.google.com/webstore/detail/watch-on-lbry/jjmbbhopnjdjnpceiecihldbhibchgek" target="_blank"><b>Watch On LBRY</b></a> extension.</li>
+        <li>Create a profile.</li>
+        <li>And set your <i>resolver API</i> as <b>Madiator Finder</b>.</li>
+        <li>Then start earning points!</li>
+    </ul>
     <Leaderboard />
 </div>
 
@@ -23,5 +28,9 @@
         min-height: 50vh;
         padding: var(--h-padding-page);
         gap: var(--h-padding);
+    }
+
+    a {
+        text-decoration: underline;
     }
 </style>
